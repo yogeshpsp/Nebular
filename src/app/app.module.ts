1bee63317bc76a8a706480838a2e9195d54063c7
yogeshpsp <yogeshwaranpsp@gmail.com>
2020-07-29 20:54:07 +0530

    header 2 done without menucontext

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbButtonModule, NbInputModule, NbIconModule, NbSelectModule, NbTabsetModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbInputModule, NbIconModule, NbSelectModule, NbTabsetModule, NbFormFieldModule, NbUserModule, NbActionsModule, NbBadgeModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { Header2Component } from './header2/header2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    Header2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,NbButtonModule,NbInputModule,NbIconModule,NbSelectModule,NbTabsetModule,
+    NbLayoutModule,NbButtonModule,NbInputModule,NbIconModule,NbSelectModule,NbTabsetModule,NbActionsModule,NbBadgeModule,NbTabsetModule,NbFormFieldModule,NbUserModule,
     NbEvaIconsModule
   ],
   providers: [],
---
+++ src/app/header2/header2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'at-header2',
+  templateUrl: './header2.component.html',
+  styleUrls: ['./header2.component.scss']
+})
+export class Header2Component implements OnInit {
+selectedItem;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
